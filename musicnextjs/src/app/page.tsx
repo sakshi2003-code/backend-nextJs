28e4716e39cee6178c867574d4fcbd511750452b
_sakshi <sakshibijalwan777@gmail.com>
2024-12-14 21:25:08 +0530

    hero-section done

--- musicnextjs/src/app/page.tsx
+++ musicnextjs/src/app/page.tsx
@@ -1,10 +1,12 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
   <main className="min-h-screen bg-black/[0.96] antialiased 
   bg-grid-white/[0.02]">
-    <h1 className="text-2xl text-center "> NextJs with Sakshi</h1>
+   
+    <HeroSection/>
   </main>
   );
 }
