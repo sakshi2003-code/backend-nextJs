a38d3e98e55894e3f0a24958dd46cef77e951029
_sakshi <sakshibijalwan777@gmail.com>
2024-12-15 19:30:51 +0530

    why choose us section done

--- musicnextjs/src/app/page.tsx
+++ musicnextjs/src/app/page.tsx
@@ -1,5 +1,8 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/MusicSchoolTestimonials";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,6 +12,9 @@ export default function Home() {
    
     <HeroSection/>
     <FeaturedCourses/>
+    <WhyChooseUs/>
+    <MusicSchoolTestimonials/>
+    <UpcomingWebinars/>
   </main>
   );
 }
